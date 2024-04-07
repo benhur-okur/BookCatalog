ffd4b4115bf58c1aa44d570966133a71c2cdb575
Doğa Güneş <120034878+doagunes@users.noreply.github.com>
2024-04-07 17:43:21 +0300

    hata düzeltildi2

--- src/main/java/com/example/bookcatalog/HelloApplication.java
+++ src/main/java/com/example/bookcatalog/HelloApplication.java
@@ -20,6 +20,7 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
 
+        System.out.println("doaaaa");
         System.out.println("merge error !");
 
         System.out.println("deneme2");
