389fb9fb1622929d0d8144b01e40bcc204826686
Doğa Güneş <120034878+doagunes@users.noreply.github.com>
2024-04-08 17:19:41 +0300

    Library class has just updated

--- src/main/java/com/example/bookcatalog/Library.java
+++ src/main/java/com/example/bookcatalog/Library.java
@@ -20,13 +20,13 @@ public class Library extends Application {
 
     Book book;
     ArrayList<Book> books = new ArrayList<>();
-    Label l1, l2, l3, l4, l5, l6;
-    TextField t1, t2, t3, t4, t5, t6;
-    HBox h1, h2, h3, h4, h5, h6,  h9;
+    Label l1, l2, l3, l4, l5, l6, l7, l8, l9, l10;
+    TextField t1, t2, t3, t4, t5, t6, t8, t10;
+    HBox h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11;
     MenuItem add;
     Button saveBttn, cancelBttn;
     Stage popupStage;
-
+    CheckBox checkSubYes, checkSubNo, checkTranslationYes, checkTranslationNo;
     @Override
     public void start(Stage stage) {
 
@@ -42,7 +42,7 @@ public class Library extends Application {
         menu.getMenus().add(file);
         mainLayout.getChildren().add(menu);
 
-        Scene scene = new Scene(mainLayout, 500, 500);
+        Scene scene = new Scene(mainLayout, 600, 600);
         stage.setScene(scene);
         stage.setTitle("BOOK CATALOG");
         stage.show();
@@ -80,14 +80,42 @@ public class Library extends Application {
         t6 = new TextField();
         h6.getChildren().addAll(l6, t6);
 
+        h7 = new HBox(8);
+        checkSubYes = new CheckBox("Yes");
+        checkSubYes.setOnAction(e -> checkSubtitle());
+        checkSubNo = new CheckBox("No");
+        checkSubNo.setOnAction(e -> checkSubtitle());
+        l7 = new Label("Subtitle?");
+        h7.getChildren().addAll(l7, checkSubYes, checkSubNo);
+
+        h8 = new HBox(8);
+        l8 = new Label("Subtitle");
+        t8 = new TextField();
+        h8.getChildren().addAll(l8, t8);
+        h8.setVisible(false);
+
         h9 = new HBox(8);
+        checkTranslationYes = new CheckBox("Yes");
+        checkTranslationYes.setOnAction(e -> checkTranslation());
+        checkTranslationNo = new CheckBox("No");
+        checkTranslationNo.setOnAction(e -> checkTranslation());
+        l9 = new Label("Translation?");
+        h9.getChildren().addAll(l9, checkTranslationYes, checkTranslationNo);
+
+        h10 = new HBox(8);
+        l10 = new Label("Translator");
+        t10 = new TextField();
+        h10.getChildren().addAll(l10, t10);
+        h10.setVisible(false);
+
+        h11 = new HBox(8);
         saveBttn = new Button("Save");
         cancelBttn = new Button("Cancel");
         cancelBttn.setOnAction(e -> cancelButton(stage));
-        h9.getChildren().addAll(saveBttn, cancelBttn);
-        popupContent.getChildren().addAll(h1,h2,h3,h4,h5,h6,h9);
+        h11.getChildren().addAll(saveBttn, cancelBttn);
+        popupContent.getChildren().addAll(h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11);
 
-        Scene scene = new Scene(popupContent, 300, 300);
+        Scene scene = new Scene(popupContent, 400, 400);
         popupStage.setScene(scene);
         popupStage.showAndWait();
 
@@ -97,6 +125,27 @@ public class Library extends Application {
         popup.show(stage);*/
     }
 
+    public void checkSubtitle(){
+
+        if(checkSubYes.isSelected()){
+            h8.setVisible(true);
+            checkSubNo.setSelected(false);
+        }else {
+            h8.setVisible(false);
+        }
+
+    }
+
+    public void checkTranslation(){
+
+        if(checkTranslationYes.isSelected()){
+            h10.setVisible(true);
+            checkTranslationNo.setSelected(false);
+        }else {
+            h10.setVisible(false);
+        }
+
+    }
 
     public void cancelButton(Stage stage){
         popupStage.close();
